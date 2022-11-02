b31103de89eaa6a5ce904a977cef53c1d44b1ccc
fbaptista <chico.baptista@gmail.com>
2022-11-02 18:55:53 -0300

    feat: implement stub method on Project that requires admin access in parent Company

--- contracts/project.sol
+++ contracts/project.sol
@@ -2,10 +2,25 @@
 
 pragma solidity ^0.8.9;
 
+import "./company.sol";
+
 contract Project {
     address public company;
 
+    modifier onlyAdmin() {
+        Company companyInstance = Company(company);
+        require(
+            companyInstance.isAdmin(msg.sender),
+            "Must be a Company admin to perform this action"
+        );
+        _;
+    }
+
     constructor(address _company) {
         company = _company;
+    }
+
+    function onlyCompanyAdminStub() public view onlyAdmin returns (bool) {
+        return true;
     }
 }
--- test/project.test.js
+++ test/project.test.js
@@ -4,10 +4,15 @@ describe("Project Contract", () => {
   let ProjectContract;
   let projectInstance;
   let companyAddress;
+  const ONLY_COMPANY_ADMIN_ERROR_MSG =
+    "Must be a Company admin to perform this action";
   before(async () => {
+    [owner, admin, maliciousAcc] = await ethers.getSigners();
+
     const CompanyContract = await ethers.getContractFactory("Company");
     const companyInstance = await CompanyContract.deploy();
     companyAddress = companyInstance.address;
+    await companyInstance.addAdmin(admin.address);
   });
   beforeEach(async () => {
     [owner, admin, maliciousAcc] = await ethers.getSigners();
@@ -19,6 +24,22 @@ describe("Project Contract", () => {
   context("on deploy", () => {
     it("should have an address of a parent Company contract", async () => {
       expect(await projectInstance.company()).to.equal(companyAddress);
+    });
+  });
+
+  describe("onlyAdminStub method", () => {
+    context("called by a registerd Admin", () => {
+      it("should return true", async () => {
+        expect(await projectInstance.connect(admin).onlyCompanyAdminStub()).to
+          .be.true;
+      });
+    });
+    context("called by a malicious account", () => {
+      it("should throw an access denied error", async () => {
+        await expect(
+          projectInstance.connect(maliciousAcc).onlyCompanyAdminStub()
+        ).to.be.revertedWith(ONLY_COMPANY_ADMIN_ERROR_MSG);
+      });
     });
   });
 });
